c7ef24255559f5f841fbcab1ce0a84ebe299e10d
김건우 <117492615+rlarjsdn0223@users.noreply.github.com>
2024-07-04 23:02:25 +0900

    v0.2
    챕터 추가 및 제출 ui 개선

--- English_vocabulary_test/public/js/input_output.js
+++ English_vocabulary_test/public/js/input_output.js
@@ -1,5 +1,5 @@
 let E_voca = ['aboard', 'abroad', 'adventure', 'aisle', 'ancient'];
-let K_voca = ['타고', '외국에', '모험', '통로', '고대의'];
+let K_voca = ['~타고', '외국에', '모험', '통로', '고대의'];
 let currentIndex = 0;
 
 // 현재 인덱스에 해당하는 단어를 설정
@@ -12,8 +12,8 @@ function setWord(index) {
 
 // 데이터 제출시 나오는 값
 function getFormData() {
-  let randomIndex = document.getElementById("E_voca").dataset.index;
-  let inputK_voca = document.getElementById("K_voca").value;
+  let randomIndex = document.getElementById("E_voca").dataset.index; // randomIndex에 선택된 단어의 인덱스를 대입
+  let inputK_voca = document.getElementById("K_voca").value; // inputK_voca에 입력 필드에서 받은 단어를 저장
 
   if (inputK_voca === K_voca[randomIndex]) {
     alert("정답입니다.");
@@ -29,9 +29,16 @@ function getFormData() {
 function getInitialConsonant(koreanChar) {
   const initialConsonants = "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ";
   const unicodeOffset = 44032; // '가'의 유니코드 값
-  const charCode = koreanChar.charCodeAt(0) - unicodeOffset;
-  const initialIndex = Math.floor(charCode / 588);
-  return initialConsonants[initialIndex];
+  const charCode = koreanChar.charCodeAt(0);
+
+  // 한글 범위 체크 ('가' <= koreanChar <= '힣')
+  if (charCode >= 44032 && charCode <= 55203) {
+    const initialIndex = Math.floor((charCode - unicodeOffset) / 588);
+    return initialConsonants[initialIndex];
+  } else {
+    // 한글이 아닌 경우 그대로 반환
+    return koreanChar;
+  }
 }
 
 // 힌트 버튼 클릭 시 호출되는 함수
--- server.js
+++ server.js
@@ -11,6 +11,23 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'English_vocabulary_test', 'public', 'English_vocabulary_test.html'));
 });
 
+// /chapter2 경로에 대한 라우팅
+app.get('/chapter2', (req, res) => {
+  res.sendFile(path.join(__dirname, 'English_vocabulary_test', 'public', 'English_vocabulary_chapter2.html'));
+});
+
+app.get('/chapter3', (req, res) => {
+  res.sendFile(path.join(__dirname, 'English_vocabulary_test', 'public', 'English_vocabulary_chapter3.html'));
+});
+
+app.get('/chapter4', (req, res) => {
+  res.sendFile(path.join(__dirname, 'English_vocabulary_test', 'public', 'English_vocabulary_chapter4.html'));
+});
+
+app.get('/chapter5', (req, res) => {
+  res.sendFile(path.join(__dirname, 'English_vocabulary_test', 'public', 'English_vocabulary_chapter5.html'));
+});
+
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
 });
